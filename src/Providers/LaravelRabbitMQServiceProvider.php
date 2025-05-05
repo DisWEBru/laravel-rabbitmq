da4fdfb8d4f84302e26da573b42fccbcfe46a912
DisWEBru <romashkov.studio@gmail.com>
2025-05-06 00:12:42 +0500

    fix: service provider, homepage

--- src/Providers/LaravelRabbitMQServiceProvider.php
+++ src/Providers/LaravelRabbitMQServiceProvider.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace DisWEB\LaravelRabbitMQ\Providers;
+namespace Diswebru\LaravelRabbitMQ\Providers;
 
 use Diswebru\LaravelRabbitMQ\Interfaces\PublishInterface;
 use Diswebru\LaravelRabbitMQ\Publish;
